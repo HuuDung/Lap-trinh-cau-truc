efde3a4416be2b7332a44230d2d7535f14503963
HuuDung <huudung2411@gmail.com>
2018-11-26 19:45:45 +0700

    fix login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
